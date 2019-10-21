ef3ac315ca684ed8f75e5d0d1394b133fad57b5a
Dolfiks <ecolton.life@gmail.com>
2019-10-22 02:13:16 +0300

    Revert "Revert "Додав 7 лабораторну""
    This reverts commit e47183b21a2bbf89962d21234870584e328e6947.

---
+++ 2 лабка/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_лабка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            double sum = 0, k, target;
+
+            Console.WriteLine("Введіть границю:");
+            target= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть крок:");
+            k = Convert.ToDouble(Console.ReadLine());
+
+            while(k<target && k>0)
+            {
+                    sum = sum + (Math.Pow(k, 2) - Math.Pow((-1), (k + 1)) * Math.Pow(k, 3)) / (Math.Pow(k, 2) + k + 1);
+                k = k + k;
+            }
+            Console.WriteLine("Сума ряду:" + sum);
+            Console.ReadKey();
+        }
+    }
+}
